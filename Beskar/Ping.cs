64c95221e9e0faf2c355c610ec6de2e634c3f280
James Kulba <jkulba@users.noreply.github.com>
2022-08-06 13:53:42 -0700

    Update Ping.cs

--- Beskar/Ping.cs
+++ Beskar/Ping.cs
@@ -1,10 +1,10 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace Beskar
@@ -14,28 +14,35 @@ namespace Beskar
         private static string _invocationId;
 
         [FunctionName("Ping")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            ExecutionContext executionContext, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ExecutionContext executionContext, ILogger log)
         {
 
             _invocationId = executionContext.InvocationId.ToString();
 
-            log.LogInformation("PING function service endpoint received a request.");
+            PingResponse pingResponse = new()
+            {
+                RequestId = _invocationId,
+                Application = "Beskar Functions",
+                StatusCode = 0,
+                Message = "Beskar Functions Ping Response",
+                CreatedDate = DateTimeOffset.UtcNow
+            };
 
-            log.LogInformation(Environment.GetEnvironmentVariable("AzureWebJobsStorage", EnvironmentVariableTarget.Process));
+            log.LogInformation("Ping request received: {pingResponse}", pingResponse);
 
             await Task.Yield();
 
-            return (ActionResult)new OkObjectResult(new
-            {
-                returnMessage = "Ping Function Response",
-                functionName = "PING",
-                invocationId = _invocationId,
-                returnStatusCode = 0,
-                returnDate = DateTimeOffset.UtcNow
-            });
-
+            return (ActionResult)new OkObjectResult(pingResponse);
         }
     }
+
+    public record PingResponse
+    {
+        public string RequestId { get; init; }
+        public string Application { get; init; }
+        public int StatusCode { get; init; }
+        public string Message { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+
 }
