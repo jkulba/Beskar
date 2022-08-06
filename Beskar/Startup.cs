055869c229db127f1ae2e605f717c0f1dc1aea20
James Kulba <jkulba@users.noreply.github.com>
2022-08-06 13:54:47 -0700

    Update Startup.cs

--- Beskar/Startup.cs
+++ Beskar/Startup.cs
@@ -1,7 +1,9 @@
 using System;
+
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using Serilog;
 
 [assembly: FunctionsStartup(typeof(Beskar.Startup))]
@@ -13,11 +15,17 @@ namespace Beskar
         public override void Configure(IFunctionsHostBuilder builder)
         {
             var logger = new LoggerConfiguration()
-              .WriteTo.Console()
-              .WriteTo.File("log.txt", rollingInterval:RollingInterval.Day)
+              .Enrich.FromLogContext()
+              .Enrich.WithThreadId()
+              .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+            //   .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
               .CreateLogger();
 
-              builder.Services.AddLogging(LoaderOptimizationAttribute => LoaderOptimizationAttribute.AddSerilog(logger));
+            builder.Services.AddLogging(LoaderOptimizationAttribute => LoaderOptimizationAttribute.AddSerilog(logger));
+
         }
     }
 }
+
+
+//    "Enrich": [ "FromLogContext", "WithMachineName", "WithThreadId", "WithExceptionDetails" ],
